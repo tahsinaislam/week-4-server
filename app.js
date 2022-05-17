7cdde39257cab60b3c478204d46a1a89061dfee8
Tahsina Islam <ti429@nyu.edu>
2022-05-17 11:34:41 -0400

    commit

--- app.js
+++ app.js
@@ -22,4 +22,42 @@ app.post('/login', (req, res) => {
   res.redirect('http://localhost:3000/login')
 })
 
+app.post('/signup', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  const password_confirmation = req.body.password_confirmation
+
+  if (!email || !password || !password_confirmation) {
+    res.status(400).send('Missing data')
+    return
+  }
+
+  if (accountExists(email)) {
+    res.status(403).send('Account already exists')
+    return
+  }
+
+  if (password !== password_confirmation) {
+    res.status(400).send('Passwords do not match')
+    return
+  }
+
+  if (password.length < 8) {
+    res.status(400).send('Password is invalid')
+    return
+  }
+
+  createAccount(email, password)
+	res.status(200).send('Account created')
+})
+
+const accountExists = (email) => {
+  //here we should check if the account exists
+  return false
+}
+
+const createAccount = (email, password) => {
+  //here we should create the account
+}
+
 app.listen(3001, () => console.log('Server ready'))
